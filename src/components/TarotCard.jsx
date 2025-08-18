58ca0ae1b52dc610163add8d95ccf00f4fd33b27
gabriela-her <ari.ghb@gmail.com>
2025-08-18 23:03:57 +0200

    feat: assets backcard image

--- src/components/TarotCard.jsx
+++ src/components/TarotCard.jsx
@@ -1,26 +1,32 @@
-export default function TarotCard({ card, faceDown = true, onClick }) { 
+import cardBack from '../assets/card-back.png'
+export default function TarotCard({ card, faceDown = true, onClick }) {
     //validacion 
-if (!card && !faceDown) {
-  console.warn('TarotCard: No card data provided for face-up card');
-}
+    if (!card && !faceDown) {
+        console.warn('TarotCard: No card data provided for face-up card');
+    }
 
-  return (
-    <button
-      className={`card ${faceDown ? 'card--back' : ''}`}
-      onClick={onClick}
-      style={{ cursor: onClick ? 'pointer' : 'default' }}
-      aria-label={faceDown ? 'Hidden card' : `Card: ${card?.arcaneName}`}
-    >
-      {faceDown || !card ? (
-        <span className="cardBack">🔮</span>
-      ) : (
-        <img
-          className="cardImg"
-          src={card.arcaneImage.imageSrc}
-          alt={card.arcaneName}
-          loading="lazy"
-        />
-      )}
-    </button>
-  );
+    return (
+        <button
+            className={`card ${faceDown ? 'card--back' : ''}`}
+            onClick={onClick}
+            style={{ cursor: onClick ? 'pointer' : 'default' }}
+            aria-label={faceDown ? 'Hidden card' : `Card: ${card?.arcaneName}`}
+        >
+            {faceDown || !card ? (
+                <img
+                    className="cardBack"
+                    src={cardBack} 
+                    alt="Dorso de la carta"
+                    loading="lazy"
+                />
+            ) : (
+                <img
+                    className="cardImg"
+                    src={card.arcaneImage.imageSrc}
+                    alt={card.arcaneName}
+                    loading="lazy"
+                />
+            )}
+        </button>
+    );
 }
