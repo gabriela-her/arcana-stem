117e978d30593f4f64958b676291e594bff73e1d
gabriela-her <ari.ghb@gmail.com>
2025-08-22 14:59:50 +0200

    fix: change L in router

--- src/router.jsx
+++ src/router.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from 'react-router-dom';
-import Layout from './components/layout';
+import Layout from './components/Layout';
 import Home from './pages/Home';
 import CardDetail from './pages/CardDetail';
 import Reading from './pages/Reading';
